d1bb6310a84e5c30f696520ef1885bc6f6b0da68
Kevin-Illu <kevinillu97@gmail.com>
2024-12-30 23:32:31 -0600

    style(only types): types for adapters and other stuffs

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,3 +1,23 @@
+// Source Like
+interface Store<T> {
+  add(item: T): boolean;
+  getAll(): T[];
+  delete(compare: (item: T) => boolean): boolean;
+  deleteAt(index: number): T | undefined;
+  find(compare: (item: T) => boolean): T | undefined;
+  update(compare: (item: T) => boolean, newValue: T): boolean;
+  size(): number;
+}
+
+interface StoreAdapter<T> {
+  add(t: TransactionEntity): Promise<boolean>;
+  getAll(): Promise<TransactionEntity[]>;
+  findById(id: number): Promise<TransactionEntity | undefined>;
+  update(t: TransactionEntity): Promise<boolean>;
+  size(): Promise<number>;
+}
+
+// Core like
 interface TransactionEntity {
   id: number;
   amount: number;

fdf8bd67e4158fab42f04643a2fb98130985d1ca
kevin-Illu <78170908+Kevin-Illu@users.noreply.github.com>
2025-12-05 22:00:46 -0600

    refactor(bun): replace node for bun
    Estoy utilzando mi propia libreria jajajaja y no sirve xD

--- src/business/menu.ts
+++ src/business/menu.ts
@@ -1,40 +1,148 @@
 import TreeMenuResolver, { type Menu } from "menu-resolver";
 import { BusinessLogic } from "../consts";
 
-const EXPENSES_ACTIONS = BusinessLogic.APPLICATION_CAPABILITIES.business.expenses.actions;
+const EXPENSES_ACTIONS =
+  BusinessLogic.APPLICATION_CAPABILITIES.business.expenses.actions;
 
-const menu: Menu[] = [
-  {
-    label: "Manage expenses",
-    resolve: BusinessLogic.APPLICATION_CAPABILITIES.app.actions.listExpensesOptions,
-    children: [
-      {
-        label: "Create a new expense",
-        resolve: EXPENSES_ACTIONS.add
+const EXPENSES_COMMANDS =
+  BusinessLogic.APPLICATION_CAPABILITIES.business.expenses.commands;
+
+const APPLICATION_ACTIONS = BusinessLogic.APPLICATION_CAPABILITIES.app.actions;
+
+type InquirerListItem = {
+  name: string;
+  value?: string;
+  description?: string;
+};
+
+export class AppMenu {
+  private tree: Menu<InquirerListItem>[] = [
+    {
+      data: {
+        name: "Manage expenses",
+        value: APPLICATION_ACTIONS.listExpensesOptions,
+        description: "Just manage your expenses",
       },
-      {
-        label: "Edit the amount of the expense",
-        resolve: EXPENSES_ACTIONS.edit
+      children: [
+        {
+          data: {
+            name: "Create a new expense",
+            value: EXPENSES_ACTIONS.add,
+          },
+          resolve: EXPENSES_COMMANDS.add,
+        },
+        {
+          data: {
+            name: "Edit the amount of the expense",
+            value: EXPENSES_ACTIONS.edit,
+          },
+          resolve: EXPENSES_ACTIONS.edit,
+        },
+        {
+          data: {
+            name: "Delete expense",
+          },
+          resolve: EXPENSES_ACTIONS.remove,
+        },
+        {
+          data: {
+            name: "List of expenses",
+          },
+          resolve: EXPENSES_ACTIONS.list,
+        },
+        {
+          data: {
+            name: "<- Go back",
+            description: "Just go back",
+          },
+          resolve: (api) => {
+            api.goBack();
+            api.goBack();
+          },
+        },
+      ],
+    },
+    {
+      data: {
+        name: "Configuration",
+        description: "Just configuration",
       },
-      {
-        label: "Delete expense",
-        resolve: EXPENSES_ACTIONS.remove
+      children: [
+        {
+          data: {
+            name: "Choose the curren storage method",
+          },
+          children: [
+            {
+              data: {
+                name: "LinkedList storage",
+                description: "This method only save change in memory",
+              },
+              resolve: (api) => {
+                // TODO: save the selection
+                api.choose(api.currentNode.id);
+                api.goBack();
+              },
+            },
+            {
+              data: {
+                name: "JSON storage",
+                description: "This method save change in a JSON file",
+              },
+              resolve: (api) => {
+                // TODO: save the selection
+                api.choose(api.currentNode.id);
+                api.goBack();
+              },
+            },
+            {
+              data: {
+                name: "SQLite Storage",
+                description: "This method save change in a SQLite database",
+              },
+              resolve: (api) => {
+                // TODO: save the selection
+                api.choose(api.currentNode.id);
+                api.goBack();
+              },
+            },
+            {
+              data: {
+                name: "<- Go back",
+                description: "Just go back",
+              },
+              resolve: ({ goBack, currentNode, choose }) => {
+                goBack();
+                goBack();
+              },
+            },
+          ],
+        },
+        {
+          data: {
+            name: "<- Go back",
+            description: "Just go back",
+          },
+          resolve: ({ goBack, currentNode, choose }) => {
+            goBack();
+            goBack();
+          },
+        },
+      ],
+    },
+    {
+      data: {
+        name: "Exit the application",
       },
-      {
-        label: "List of expenses",
-        resolve: EXPENSES_ACTIONS.list
-      },
-      {
-        label: "<- Go back",
-        resolve: ({ goBack }) => goBack(),
-      }
-    ]
-  },
-  {
-    label: "Exit the application",
-    resolve: BusinessLogic.APPLICATION_CAPABILITIES.app.actions.exit
+      resolve: () => process.exit(),
+    },
+  ];
+
+  menu: TreeMenuResolver;
+
+  constructor() {
+    this.menu = new TreeMenuResolver(this.tree);
   }
-];
+}
 
-const AppMenu = new TreeMenuResolver(menu);
 export default AppMenu;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
+import TreeMenuResolver from "menu-resolver";
+import AppMenu from "./business/menu";
+import { select } from "@inquirer/prompts";
 
 class Application {
+  constructor(private menu: TreeMenuResolver) {}
+
   async start() {
-    console.log("Starting app...");
+    while (true) {
+      const options = this.menu.getDisplayableMenu();
+      const selectableMenu = this.getSelectableMenu(options);
+      const result = await this.showSelect(selectableMenu);
+      const action = this.menu.choose(result);
+
+      if (typeof action?.resolve === "function") {
+        action.resolve();
+      }
+    }
+  }
+
+  async showSelect(options: any): Promise<string> {
+    const result = await select({
+      message: "",
+      choices: options,
+    });
+
+    return result as string;
+  }
+
+  getSelectableMenu(options: { id: string; data?: any }[]) {
+    return options.map((o) => ({ ...o?.data, value: o.id }));
   }
 }
 
-new Application().start();
+const menu = new AppMenu().menu;
+
+new Application(menu).start();
